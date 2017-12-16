edb536b9b37335c313b0a93d760168a6dc9f7a26
Lucas Dugny <lucas.dugny9@etu.univ-lorraine.fr>
2017-12-16 11:40:12 +0100

    startpage

--- app/src/main/java/com/example/ghislainmertz/spotinder/MainActivity.java
+++ app/src/main/java/com/example/ghislainmertz/spotinder/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.ghislainmertz.spotinder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.imageButton2);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Identification.class);
+                startActivity(intent);
+            }
+        });
     }
 }
